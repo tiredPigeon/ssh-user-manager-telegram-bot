98cb1c6545cbe45711a9752d727daaa6adcc9fd7
tiredPigeon <tiredPigeon@telegmail.com>
2023-09-06 06:03:42 +0330

    Changes to be committed: 	new file:   .eslintrc.js 	modified:   .github/workflows/eslint.yml 	modified:   .gitignore 	new file:   eslint-results.sarif 	modified:   package.json 	new file:   src/localization/fa.json 	modified:   src/localization/index.ts 	modified:   tsconfig.json

--- src/localization/index.ts
+++ src/localization/index.ts
@@ -2,6 +2,7 @@ import LanguageState from "../states/language";
 
 const locales = {
   en: require("./en.json"),
+  fa: require("./fa.json")
 };
 
 export const setLanguage = (language: string) => {
@@ -12,10 +13,10 @@ export const t = (textKey: string) => {
   let language = LanguageState.getState();
   if (!Object.keys(locales).includes(language)) {
     language = "en";
-  }
-
-  if (locales.en[textKey]) {
+  }else if (locales.en[textKey]) {
     return locales.en[textKey];
+  }else if(locales.fa[textKey]){
+return locales.fa[textKey];
   } else {
     return textKey;
   }
